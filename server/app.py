31923c4e1ea20bc764985e5c85a7b1f5131d0a11
Kirill Savin <krllssrgv@gmail.com>
2024-07-18 19:57:02 +0300

    JWT

--- server/app.py
+++ server/app.py
@@ -83,87 +83,85 @@ user_login_model = user_api.model('user_login', {
 @user_api.route('/register')
 class Register(Resource):
     def post(self):
-        if (current_user.is_authenticated):
-            return '', 403
+        data = request.json
+        check = True
+        result = {}
+
+        if (users.query.filter(users.email == data['email']).all()):
+            check = False
+            result['email'] = 'Пользователь уже существует'
+
+        if (len(data['password']) < 6):
+            check = False
+            result['password'] = 'Минимальная длина 6 символов'
+
+        if (data['password'] != data['repeatedPassword']):
+            check = False
+            result['repeatedPassword'] = 'Пароли не совпадают'
+
+        if check:
+            new_user = users(
+                email=data['email'],
+                password=generate_password_hash(data['password']),
+                name=data['name'],
+                surname=data['surname']
+            )
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                return '', 204
+            except:
+                return '', 500
         else:
-            data = request.json
-            check = True
-            result = {}
-
-            if (users.query.filter(users.email == data['email']).all()):
-                check = False
-                result['email'] = 'Пользователь уже существует'
-
-            if (len(data['password']) < 6):
-                check = False
-                result['password'] = 'Минимальная длина 6 символов'
-
-            if (data['password'] != data['repeatedPassword']):
-                check = False
-                result['repeatedPassword'] = 'Пароли не совпадают'
-
-            if check:
-                new_user = users(
-                    email=data['email'],
-                    password=generate_password_hash(data['password']),
-                    name=data['name'],
-                    surname=data['surname']
-                )
-                try:
-                    db.session.add(new_user)
-                    db.session.commit()
-                    return '', 204
-                except:
-                    return '', 500
-            else:
-                return result, 400
+            return result, 400
 
 
 @user_api.route('/login')
 class Login(Resource):
-    @user_api.header('Access-Control-Allow-Credentials', 'true')
-    @user_api.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    # @user_api.header('Access-Control-Allow-Credentials', 'true')
+    # @user_api.header('Access-Control-Allow-Origin', 'http://localhost:3000')
     @user_api.expect(user_login_model)
     def post(self):
-        if (current_user.is_authenticated):
-            return '', 400
-        else:
-            data = request.json
-            user = users.query.filter_by(email=data['email']).first()
+        data = request.json
+        user = users.query.filter_by(email=data['email']).first()
 
-            if user:
-                if check_password_hash(user.password, data['password']):
-                    access_token = create_access_token(identity={'user_id': user.id})
-                    print(access_token)
-                    return '', 200, {'Set-Cookie': f'access_token_cookie={access_token}; HttpOnly; SameSite=None; Secure=False; Path=/; Max-Age={int(timedelta(days=30).total_seconds())}'}
-                else:
-                    return {'password': 'Неправильный пароль'}, 401
+        if user:
+            if check_password_hash(user.password, data['password']):
+                access_token = create_access_token(identity={'user_id': user.id})
+                return '', 200, {
+                    'Set-Cookie': f'access_token_cookie={access_token}; HttpOnly; SameSite=None; Secure=False; Path=/; Max-Age={int(timedelta(days=30).total_seconds())}'
+                }
             else:
-                return {'email': 'Нет такого пользователя'}, 401
+                return {'password': 'Неправильный пароль'}, 401
+        else:
+            return {'email': 'Нет такого пользователя'}, 401
 
 
 @user_api.route('/logout')
 class Logout(Resource):
+    @jwt_required
     def post(self):
-        logout_user()
-        return '', 200
+        user_id = get_jwt_identity()['user_id']
+        user = db.session.get(users, user_id)
+        if (user):
+            return '', 200, {
+                'Set-Cookie': 'access_token_cookie=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Path=/'
+            }
+        else:
+            return '', 401
     
 
 @user_api.route('/check_login')
 class CheckLogin(Resource):
     @jwt_required()
     def get(self):
-        # if (current_user.is_authenticated):
-        #     return '', 200
-        # else:
-        #     return '', 401
-        user = get_jwt_identity()
-        print(user)
-        return '', 401
-        
+        user_id = get_jwt_identity()['user_id']
+        user = db.session.get(users, user_id)
 
-# response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-# response.headers['Access-Control-Allow-Credentials'] = 'true'
+        if (user):
+            return '', 204
+        else:
+            return '', 401
 
 
 @act_api.route('/get_acts')
@@ -206,60 +204,55 @@ class GetActs(Resource):
 
 @act_api.route('/set_day_as_done')
 class DoneDay(Resource):
+    @jwt_required
     def post(self):
-        if (current_user.is_authenticated):
+        user_id = get_jwt_identity()['user_id']
+        user = db.session.get(users, user_id)
+        if (user):
             data = request.json
-            user = db.session.get(users, int(current_user.get_id()))
-            if (user):
-                if ('set_day' in data):
-                    if (data['set_day'] == 1):
-                        user.day_one = True
-
-                    elif (data['set_day'] == 2):
-                        user.day_two = True
-
-                    elif (data['set_day'] == 3):
-                        user.day_three = True
+            if ('set_day' in data):
+                if (data['set_day'] == 1):
+                    user.day_one = True
+                elif (data['set_day'] == 2):
+                    user.day_two = True
+                elif (data['set_day'] == 3):
+                    user.day_three = True
                     
-                    if (user.day_one and user.day_two and user.day_three):
-                        user.success = '0'
+                if (user.day_one and user.day_two and user.day_three):
+                    user.success = '0'
 
-                    try:
-                        db.session.commit()
-                        return '', 200
-                    except:
-                        return '', 500
-                else:
-                    return '', 400
-            
+                try:
+                    db.session.commit()
+                    return '', 200
+                except:
+                    return '', 500
+            else:
+                    return '', 400 
         else:
             return '', 401
 
 
 @act_api.route('/send_result')
 class Result(Resource):
+    @jwt_required
     def post(self):
-        if (current_user.is_authenticated):
+        user_id = get_jwt_identity()['user_id']
+        user = db.session.get(users, user_id)
+        if (user):
             data = request.json
-            user = db.session.get(users, int(current_user.get_id()))
-            if (user):
-                if ('success' in data):
-                    if (data['success']):
-                        user.success = '1'
-                    else:
-                        user.success = '-1'
-                    
-                    try:
-                        db.session.commit()
-                        return {'success': user.success}, 200
-                    except:
-                        return '', 500
-                    
+            if ('success' in data):
+                if (data['success']):
+                    user.success = '1'
                 else:
-                    return '', 400
-
+                    user.success = '-1'
+                    
+                try:
+                    db.session.commit()
+                    return {'success': user.success}, 200
+                except:
+                    return '', 500 
             else:
-                return '', 401
+                return '', 400
         else:
             return '', 401
         
